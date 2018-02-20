7484f1c4513f94210ae1269090b0869090a863c2
saidghamra <35788699+saidghamra@users.noreply.github.com>
2018-02-20 13:07:48 -0500

    Add files via upload

--- StudentInfo.java
+++ StudentInfo.java
@@ -1,3 +1,9 @@
+// Author: Said Ghamra, Andrew Dzierzanowski
+// Student number: 300008217, 300002426 
+// Course: ITI 1121-B00
+// Assignment: 1
+// Question: 1.1
+
 /**
  * Contains a method <code>void display()</code> that all the <code>main</code> methods
  * call to display the student information. Fill the box with your personal
@@ -16,12 +22,13 @@ public class StudentInfo {
     public static void display() {
 
 	System.out.println("************************************************************");
-	System.out.println("*                                                          *");
-	System.out.println("*                                                          *");
-	System.out.println("*                                                          *");
-	System.out.println("*                                                          *");
+	System.out.println("* Said Ghamra - Andrew Dzierzanowski	                   *");
+	System.out.println("* 300008217 -  300002426                                   *");
+	System.out.println("* ITI1121B                                                 *");
+	System.out.println("* Assignment #1                                            *");
 	System.out.println("************************************************************");
 	System.out.println();
+
 
     }
 
